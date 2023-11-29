1248b5484491c4153924ad0d30c75c88fc39e50d
Paul Sisson <paulsissonsemail@gmail.com>
2023-11-29 14:21:08 -0600

    Begin working with sphere

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import styles from './page.module.css'
 import { Canvas } from '@react-three/fiber'
 import Box from '@/components/box/Box'
+import Sphere from '@/components/sphere/Sphere';
 
 export default function Home() {
   return (
@@ -12,6 +13,11 @@ export default function Home() {
         <pointLight position={[10, 10, 10]} />
         <Box position={[-1.2, -1, 1]} />
         <Box position={[1.2, 0, 0]} />
+      </Canvas>
+      <Canvas>
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+        <Sphere position={[1, 1, .2]} />
       </Canvas>
     </main>
   )
